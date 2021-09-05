9011ce49fa5364ae20b842b06905ffd9346f7c79
Paul <paul.kirn@yandex.ru>
2021-09-05 21:16:40 +0300

    polyclinic

--- Program.cs
+++ Program.cs
@@ -10,22 +10,20 @@ namespace СSLight
     {
         static void Main(string[] args)
         {
-            int money;
-            int crystalsCount;
-            int crystalPrice = 20;
-            bool enoughMoney;
+            int people;
+            int timeOfreceipt = 10;
 
-            Console.WriteLine($"Здравствуй, путник! В моей лавке все кристалы стоят {crystalPrice} золотых монет. Сколько у тебя монет?");
-            Console.Write("Ваш ответ: ");
-            money = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Прекрасно! Сколько кристалов тебе нужно?");
-            Console.Write("Ваш ответ: ");
-            crystalsCount = Convert.ToInt32(Console.ReadLine());
-            enoughMoney = money >= crystalsCount * crystalPrice;
-            crystalsCount *= Convert.ToInt32(enoughMoney);
-            money -= crystalsCount * crystalPrice;
-            Console.WriteLine($"У вас осталось {money} золотых монет");
-            Console.WriteLine($"Вы получили {crystalsCount} кристалов");
+            Console.WriteLine("Фиксированное время приема одного человека 10 минут.");
+            Console.Write("Введите количество людей в очереди: ");
+           
+            people = Convert.ToInt32(Console.ReadLine());
+
+            int totalTime = people * timeOfreceipt;
+            int hour = totalTime / 60;
+            int minutes = totalTime % 60;
+
+            Console.WriteLine($"Вы должны отстоять в очереди {hour} часа и {minutes} минут.");
+
 
 
         }
