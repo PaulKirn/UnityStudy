17a215b576d315f73c4570f72a414859a186845e
Paul <paul.kirn@yandex.ru>
2021-09-05 17:35:10 +0300

    variables

--- Program.cs
+++ Program.cs
@@ -10,8 +10,27 @@ namespace СSLight
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Привет, мир!");
-            Console.ReadKey();
+            //Основные типы: int, uint | float | char | string
+            //Целочисленные
+            byte b; // 0 to 255
+            sbyte sb; // -128 to 127
+            short sh; // -32768 to 32767
+            ushort us; // 0 to 65535
+            int i; // -2147483648 to 2147483647
+            uint ui; // 0 to 4294967295
+            long ln; // a lot
+            ulong ul;
+            // Числа с плавующей точкой
+            float fl = 5.7f; // 7 symbals
+            double db = 5.7; // 15 symbols
+            // Символьный тип
+            char ch = 'd';
+            // Строковые
+            string str = "Привет, как дела?";
+            // Логическая переменная
+            bool bl = true;
+
+            int age = 5;
         }
     }
 }
